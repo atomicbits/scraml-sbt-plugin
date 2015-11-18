07a0b321624897c6a7a036a11b136ba57a7409e3
Peter Rigole <peter.rigole@atomicbits.io>
2015-11-18 22:01:36 +0100

    Version bump to 0.4.3-SNAPSHOT.

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -35,7 +35,7 @@ object ScramlSbtPlugin extends AutoPlugin {
 
   override def buildSettings: Seq[Setting[_]] = Seq(
     libraryDependencies ++= Seq(
-      "io.atomicbits" %% "scraml-dsl-scala" % "0.4.2" withSources() withJavadoc()
+      "io.atomicbits" %% "scraml-dsl-scala" % "0.4.3-SNAPSHOT" withSources() withJavadoc()
     )
   )
 
