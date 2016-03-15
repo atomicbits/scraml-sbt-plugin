36f69454641d6f9c368eda2d4822424235ab43ba
Peter Rigole <peter.rigole@atomicbits.io>
2016-03-15 12:21:30 +0100

    Make de develop branch build again, the 0.4.11-SNAPSHOT of the scraml generated is not yet published.

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -44,7 +44,7 @@ object ScramlSbtPlugin extends AutoPlugin {
 
     def generateExtraBuildSettings: Seq[Setting[_]] = {
 
-      val version = "0.4.11-SNAPSHOT"
+      val version = "0.4.10"
 
       language match {
         case "java" =>
