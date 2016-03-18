68f9f40b8d76c3c65e4c58f4749694188e2ec64c
Peter Rigole <peter.rigole@atomicbits.io>
2016-03-18 14:25:37 +0100

    Develop of the sbt plugin may point to the latest release of scraml generator.

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -44,7 +44,7 @@ object ScramlSbtPlugin extends AutoPlugin {
 
     def generateExtraBuildSettings: Seq[Setting[_]] = {
 
-      val version = "0.4.12-SNAPSHOT"
+      val version = "0.4.11"
 
       language match {
         case "java" =>
