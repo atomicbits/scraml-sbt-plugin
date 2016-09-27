4c532abf13eadc88a8c96baaaa689abb3aa1c4b8
Peter Rigole <peter.rigole@atomicbits.io>
2016-09-27 07:42:09 +0200

    Preparing for release 0.4.17.

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -59,7 +59,7 @@ object ScramlSbtPlugin extends AutoPlugin {
 
     def generateExtraBuildSettings: Seq[Setting[_]] = {
 
-      val version = "0.4.17-SNAPSHOT"
+      val version = "0.4.17"
 
       scramlVersion := version
 
