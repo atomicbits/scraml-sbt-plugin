58b6abaaa4d177668442182744077175bfe0d5cb
Peter Rigole <peter.rigole@atomicbits.io>
2017-05-18 10:54:59 +0200

    Unofficial release 0.6.1d

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -57,7 +57,7 @@ object ScramlSbtPlugin extends AutoPlugin {
 
     def generateExtraBuildSettings: Seq[Setting[_]] = {
 
-      val version = "0.6.1d-SNAPSHOT"
+      val version = "0.6.1d"
 
       scramlVersion := version
 
