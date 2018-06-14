f4c91c5ba9b63270e2cc5bb2d6b5bff967749972
Peter Rigole <peter.rigole@atomicbits.io>
2018-06-14 08:40:54 +0200

    Preparing for release 0.8.2.

--- src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
+++ src/main/scala/io/atomicbits/scraml/sbtplugin/ScramlSbtPlugin.scala
@@ -1,16 +1,17 @@
 /*
  *
- *  (C) Copyright 2015 Atomic BITS (http://atomicbits.io).
+ * (C) Copyright 2018 Atomic BITS (http://atomicbits.io).
  *
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the GNU Affero General Public License
- *  (AGPL) version 3.0 which accompanies this distribution, and is available in
- *  the LICENSE file or at http://www.gnu.org/licenses/agpl-3.0.en.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *  Affero General Public License for more details.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  *  Contributors:
  *      Peter Rigole
@@ -71,7 +72,7 @@ object ScramlSbtPlugin extends AutoPlugin {
     val scramlLanguage = settingKey[String]("scraml language setting, deprecated: use platform instead")
     val scramlPlatform = settingKey[String]("scraml platform setting (defaults to ScalaPlay)")
     val scramlClasPathResource = settingKey[Boolean]("indicate that raml files are located in a classpath resource (default is false)")
-    val scramlLicenseKey = settingKey[String]("scraml 3rd party license key")
+    val scramlLicenseKey = settingKey[String]("scraml 3rd party license key (no longer used)")
     val scramlClassHeader = settingKey[String]("scraml 3rd party class header")
     val scramlVersion = settingKey[String]("scraml version")
     val scramlDestinationDir = settingKey[File]("scraml generated source output dir, default is target/scala-<version>/src_managed/")
